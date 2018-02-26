561c41b5325d094710217e35588b44c4b419512e
Kailin Cannon <kailin.cannon@gmail.com>
2018-02-26 15:34:19 -0700

    Add sevenhour and tenday card tests

--- test/SevenHour.test.js
+++ test/SevenHour.test.js
@@ -2,16 +2,32 @@ import React from 'react';
 import { mount, shallow} from 'enzyme';
 import SevenHour from '../lib/scripts/SevenHour';
 import Card from '../lib/scripts/Card';
+import mockData from '../lib/scripts/MockData';
 
 
-describe.skip('Seven Hour Cards', () => {
+describe('Seven Hour Card', () => {
   let wrapper;
+  const stuff = mockData.hourly_forecast;
 
   beforeEach(() => {
-    wrapper = shallow(<SevenHour />)
+    wrapper = shallow(<SevenHour data={stuff}/>);
   })
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
-  })
+  });
+
+  it('should return 7 cards', () => {
+    wrapper = mount(<SevenHour data={stuff} />);
+
+    expect(wrapper.children().length).toEqual(7);
+  });
+
+  it('should have the data on cards', () => {
+    wrapper = mount(<SevenHour data={stuff} />)
+
+    expect(wrapper.find('h5').first().text()).toEqual('12:00 PM')
+    expect(wrapper.find('img').first().prop('src')).toEqual('http://icons.wxug.com/i/c/k/partlycloudy.gif')
+    expect(wrapper.find('h5').last().text()).toEqual('Temp: 45℉')
+  });
 })
--- test/TenDay.test.js
+++ test/TenDay.test.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { mount, shallow} from 'enzyme';
 import TenDay from '../lib/scripts/TenDay';
 import Card from '../lib/scripts/Card';
+import mockData from '../lib/scripts/MockData';
 
 
-describe.skip('Ten Day Cards', () => {
+describe.only('Ten Day Cards', () => {
   let wrapper;
+  const mockWeather = mockData.forecast.simpleforecast.forecastday;
 
   beforeEach(() => {
-    wrapper = shallow(<TenDay />)
+    wrapper = mount(<TenDay data={mockWeather}/>)
   })
 
   it('should exist', () => {
     expect(wrapper).toBeDefined();
+  })
+
+  it('should return 10 cards', () => {
+    expect(wrapper.children().length).toEqual(10);
+  });
+
+  it('should have the data on cards', () => {
+
+    expect(wrapper.find('h5').first().text()).toEqual('Wednesday')
+    expect(wrapper.find('img').first().prop('src')).toEqual('http://icons.wxug.com/i/c/k/partlycloudy.gif')
+    expect(wrapper.find('h5').last().text()).toEqual('High/Low: 18/31')
   })
 })
